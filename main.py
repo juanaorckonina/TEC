ad7c7155e388e39a7b3745631aafd13e8af85bce
juanaorckonina <juanaorcko5@gmail.com>
2025-07-16 10:55:42 +0000

    tec

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-numero=-3.1416
-pi=-numero
-print(pi)
+print("ingrese un numero")
+numero1=int(input("ingrese un numero"))
+numero2=int(input("ingrese otro numero"))
+suma=(numero1+numero2)
+print("la suma de estos numeros da",suma)
+menor = numero1 < numero2
+menor = numero1 > numero2
+igual = numero1 == numero2
+
+
+#print("es mayor que ",suma)
+
+if mayor:
+  print("numero1 es mayor que numero2")
+if igual:
+  print("numero1 es iguai que numero2")
+if menor:
+  print("numero1 es menor que numero2")
+
+ lista = [0,1,2,3,4,5,6,7]
+for elemento in lista [:]:
+  lista[elemento]=elemento*2
+  print (elemento[elemento],end=",")
+print("")   
+
+for x in range(10):
+ print(x)
+ 
+
